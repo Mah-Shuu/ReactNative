56f8e6fd5e3eaa487ff780737feb0ad1f97fb1b0
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-05-08 17:13:32 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,17 @@
-import { Text, View, Image, TouchableOpacity} from "react-native";
-
-function correr(){
-  alert("Lhe falta ódio...\nMorra pelas minhas mões!")
-}
+import { useState } from "react";
+import { Text, View, TouchableOpacity, TextInput} from "react-native";
 
 export default function Index() {
+
+  const [nome, setNome] = useState("");
+
   return (
-    <View>
-      <View className="h-16 bg-orange-600 flex justify-center">
-        <Text className="ml-3 text-xl text-white">Detalhes do Produto</Text>
-      </View>
-      <View className="flex items-center">
-        <Image className="object-cover h-72 w-72 mt-5" source={{uri: "https://brainrottoy.com/cdn/shop/files/Tung-tung-tung-sahur-brainrot-toy-buy-brainrottoy-plushie-png.png?v=1744919619"}}/>
-        <View className="flex w-full">
-          <View className="m-8">
-            <Text className="text-3xl font-bold">Tung Tung Tung Sahur Plushie</Text>
-            <Text className="mt-2">Tung tung tung tung sahur. Anomali mengerikan yang hanya keluar pada sahur. Konon katanya kalau ada orang yang dipanggil Sahur tiga kali dan tidak nyaut maka makhluk ini datang di rumah kalian. Hi seremnya. Tung tung ini biasanya bersuara layaknya pukulan kentungan seperti ini. Share ke teman kalian yang susah Sahur.</Text>
-            <Text className="font-bold text-3xl mt-3 mb-3 text-emerald-500">R$ 200.00</Text>
-            <TouchableOpacity className="bg-orange-600 text-white font-bold rounded-full flex items-center justify-center p-4">Comprar Agora</TouchableOpacity>
-          </View>
-        </View>
-      </View>
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="text-white">{nome}</Text>
+      <TextInput className="bg-white" onChangeText={(e) => setNome(e)}/>
+      <TouchableOpacity className="bg-slate-700 p-4">
+        <Text className="text-white">Click</Text>
+      </TouchableOpacity>
     </View>
   );
 }
