b20dc8c51a21f9b595615bb826fdaebd68d05d18
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-05-12 15:07:31 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View, Image, TouchableOpacity, ScrollView} from "react-native";
 
 export default function Index() {
   return (
-    <View className="bg-slate-100">
+    <View className="bg-slate-100 h-screen">
       <View className="h-16 bg-orange-600 flex justify-center">
         <Text className="ml-3 font-bold text-xl text-white">Dashboard</Text>
       </View>
@@ -26,7 +26,8 @@ export default function Index() {
       </View>
 
       <ScrollView>
-        <View className="m-3">
+        <View className="m-3 mt-0 pb-16 gap-2">
+
           <View className="flex items-center bg-white rounded drop-shadow-lg p-4 gap-3">
             <Image className="object-cover h-60 w-60" source={{uri: "https://brainrottoy.com/cdn/shop/files/Tung-tung-tung-sahur-brainrot-toy-buy-brainrottoy-plushie-png.png?v=1744919619"}}/>
 
@@ -38,17 +39,29 @@ export default function Index() {
 
           </View>
           <View className="flex items-center bg-white rounded drop-shadow-lg p-4 gap-3">
-            <Image className="object-cover h-60 w-60" source={{uri: "https://brainrottoy.com/cdn/shop/files/Tung-tung-tung-sahur-brainrot-toy-buy-brainrottoy-plushie-png.png?v=1744919619"}}/>
+            <Image className="object-cover h-60 w-60" source={{uri: "https://tralalerotralalaplushies.com/wp-content/uploads/2025/04/Tralalero-Tralala-Plush.png"}}/>
 
             <View className="gap-2">
-              <Text className="text-xl font-bold">Tung Tung Tung Sahur Plushie</Text>
-              <Text>Tung tung tung tung sahur. Anomali mengerikan yang hanya keluar pada sahur. Konon katanya kalau ada orang yang dipanggil Sahur tiga kali dan tidak nyaut maka makhluk ini datang di rumah kalian. Hi seremnya. Tung tung ini biasanya bersuara layaknya pukulan kentungan seperti ini. Share ke teman kalian yang susah Sahur.</Text>
+              <Text className="text-xl font-bold">Tralalero Tralala Plushie</Text>
+              <Text>Trallallero-trallalà, porco Dio e porco Allah Ero con il mio fottuto figlio merdardo a giocare a Fortnit Quando a un punto arriva mia nonna, Ornella Leccacappella.</Text>
+              <TouchableOpacity className="bg-orange-600 text-white font-bold rounded-full flex items-center justify-center p-2">Ver Mais</TouchableOpacity>
+            </View>
+
+          </View>
+          <View className="flex items-center bg-white rounded drop-shadow-lg p-4 gap-3">
+            <Image className="object-cover h-60 w-60" source={{uri: "https://images.tcdn.com.br/img/img_prod/1119462/pelucia_patrick_35cm_multikids_99636971_1_986672a908f5b2fd1db2defcfa8a1fd5.jpg"}}/>
+
+            <View className="gap-2">
+              <Text className="text-xl font-bold">Patrick Plushie</Text>
+              <Text>Olha quem saiu da TV para o mundo real! O Bob Esponja e Patrick de pelúcia!! Com plush super premium macio e detalhes bordados, são mega fofinhos e representam os moradores marinhos mais famosos do mundo! Detalhe bordado Plush super premium macio Hipoalérgico Enchimento em poliéster *Imagens Meramente Ilustrativas*</Text>
               <TouchableOpacity className="bg-orange-600 text-white font-bold rounded-full flex items-center justify-center p-2">Ver Mais</TouchableOpacity>
             </View>
 
           </View>
         </View>
-      </ScrollView> 
+      </ScrollView>
+
+
     </View>
   );
 }
