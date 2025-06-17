a1883d1a22c78038835e643f1127af45a6d38543
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-06-17 17:08:37 -0400

    cabei quase só ta bugado em baixo

--- app/view/home.tsx
+++ app/view/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Home(){
 
@@ -37,15 +37,17 @@ export default function Home(){
 
 
     return(
-        <View className="flex-1 bg-slate-100">
-            <ScrollView>
+        <View className="flex-1  bg-slate-100">
+            <ScrollView className="h-screen ">
                 {produtos.map((item, index) => (
-                    <View key={index} className="m-3 flex-row w-full box-content">
-                        <Image resizeMode="contain" className=" w-10 h-10 items-center justify-center" source={{uri: item.image}}></Image>
-                        <View className="flex w-full">
-                            <Text className="text-lg font-bold mt-2 text-wrap w-fit">{item.title}</Text>
-                            <Text className="">{item.category}</Text>
-                            <Text className="text-green-500 font-bold mt-2">R$ {item.price}</Text>
+                    <View key={index} className="m-2 flex-row box-border items-center text-wrap gap-3 bg-white p-2 rounded-xl">
+                        <Image resizeMode="contain" className=" w-32 h-32 items-center justify-center" source={{uri: item.image}}></Image>
+                        <View className="flex justify-between text-wrap w-full h-full" style={{flexShrink: 1}}>
+                            <View className="flex text-wrap" >
+                                <Text className="text-base font-bold text-wrap">{item.title}</Text>
+                                <Text className="text-green-500 font-bold mt-1">R$ {item.price}</Text>
+                            </View>
+                            <TouchableOpacity className="bg-black text-white text-center mt-3 box-border p-2 rounded-xl">Detalhes</TouchableOpacity>
                         </View>
                     </View>
                 ))}
